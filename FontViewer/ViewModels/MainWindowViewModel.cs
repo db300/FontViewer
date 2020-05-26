e5a7cd0d16a042525f3586b096577fbee72cb581
DavidHawkes <db300@163.com>
2020-05-26 18:21:55 +0800

    增加字体读取类库

--- FontViewer/ViewModels/MainWindowViewModel.cs
+++ FontViewer/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reactive;
 using System.Text;
 using Avalonia;
@@ -21,16 +22,35 @@ namespace FontViewer.ViewModels
 
         #region property
 
+
+
+        #endregion
+
+        #region command
+
         public ReactiveCommand<Unit, Unit> OpenCommand => ReactiveCommand.CreateFromTask(async () =>
         {
-            var result = await new OpenFileDialog{Filters = {new FileDialogFilter{Name = ".otf",Extensions = new List<string>{ "otf" } }}}.ShowAsync((Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow);
-            if (result?.Length > 0)
+            var openDlg = new OpenFileDialog
             {
-                System.Diagnostics.Debug.WriteLine(result);
+                AllowMultiple = true,
+                Filters =
+                {
+                    new FileDialogFilter {Name = "字体文件(*.otf;*.ttf)", Extensions = new List<string> {"otf", "ttf"}},
+                    new FileDialogFilter {Name = "OpenType文件(*.otf)", Extensions = new List<string> {"otf"}},
+                    new FileDialogFilter {Name = "TrueType文件(*.ttf)", Extensions = new List<string> {"ttf"}},
+                    new FileDialogFilter {Name = "所有文件(*.*)", Extensions = new List<string> {"*"}}
+                }
+            };
+            var result = await openDlg.ShowAsync((Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow);
+            if (!(result?.Length > 0)) return;
+            System.Diagnostics.Debug.WriteLine(result);
+            foreach (var fileName in result)
+            {
+                await using var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                var typeface = new WaterTrans.GlyphLoader.Typeface(fs);
+                System.Diagnostics.Debug.WriteLine(typeface);
             }
         });
-
-        public ReactiveCommand<Unit, Unit> ExitCommand => ReactiveCommand.Create(() => { (Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.Shutdown(); });
 
         #endregion
     }
